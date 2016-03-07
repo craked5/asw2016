8e59d3e80ea9d7310bcc48d1612c8160a8e415cf
nunosilva <craked5@gmail.com>
2016-03-07 11:27:38 +0000

    alterei asw.py

--- asw.py
+++ asw.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
-from flask.ext.mysql import MySQL
+#from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
 
-mysql = MySQL()
+#mysql = MySQL()
 
+'''
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'halflife2'
@@ -18,7 +19,7 @@ cursor = conn.cursor()
 cursor.execute("INSERT INTO utilizadores (`user_id`, `nick`, `nome`, `apelido`, `email`, `pais`, `concelho`, `distrito`, `data_reg`) VALUES ('3', 'wad', 'awd', 'awd', 'awdawd', 'awd', 'awd', 'awd', '2016-03-03')")
 users = cursor.fetchall()
 print users
-
+'''
 @app.route('/')
 def leiloes():
     return render_template('leiloes.html')
@@ -33,12 +34,12 @@ def register():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
-
+    '''
     cursor = conn.cursor()
     cursor.execute("INSERT INTO utilizadores (`user_id`, `nick`, `nome`, `apelido`, `email`, `pais`, `concelho`, `distrito`, `data_reg`) VALUES ('3', 'wad', 'awd', 'awd', 'awdawd', 'awd', 'awd', 'awd', '2016-03-03')")
     users = cursor.fetchall()
     print users
-
+    '''
     return render_template('login_register.html')
 
 if __name__ == '__main__':
