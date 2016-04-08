eb34f38d034d4bbfd026b4d76a372d10289c3f91
nunosilva <craked5@gmail.com>
2016-04-08 12:17:17 +0100

    added preliminary stuff for auctions

---
+++ asw44285.sql
@@ -0,0 +1,151 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: asw44285
+-- ------------------------------------------------------
+-- Server version	5.6.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `artigos`
+--
+
+DROP TABLE IF EXISTS `artigos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `artigos` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `designacao` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `valor_base` decimal(10,2) NOT NULL,
+  `descricao` text COLLATE utf8_unicode_ci,
+  `data_entr` datetime DEFAULT NULL,
+  `data_fim` datetime DEFAULT NULL,
+  `melhor_lic` int(11) DEFAULT NULL,
+  `melhor_val` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `imagens`
+--
+
+DROP TABLE IF EXISTS `imagens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `imagens` (
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `nome_img` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`item_id`,`nome_img`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `licitacoes`
+--
+
+DROP TABLE IF EXISTS `licitacoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `licitacoes` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `data_licit` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `valor` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`item_id`,`data_licit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `palavraschave`
+--
+
+DROP TABLE IF EXISTS `palavraschave`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `palavraschave` (
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `pal_chave` varchar(16) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`item_id`,`pal_chave`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `perguntas`
+--
+
+DROP TABLE IF EXISTS `perguntas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `perguntas` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `data_perg` datetime NOT NULL,
+  `data_resp` datetime NOT NULL,
+  `pergunta` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `resposta` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`item_id`,`data_perg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `tag_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `tag` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utilizadores`
+--
+
+DROP TABLE IF EXISTS `utilizadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilizadores` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin` tinyint(1) DEFAULT '0',
+  `nick` char(12) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nome` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `apelido` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pais` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `concelho` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `distrito` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foto` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data_reg` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `nick` (`nick`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-08 12:16:22
